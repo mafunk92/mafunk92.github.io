a5e472767a3a3563676adfc2bb60742b26b674c6
Michael Funk <mafunk92@gmail.com>
2015-06-12 17:59:54 -0700

    y

--- ngBlog/app/scripts/controllers/post.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-
-angular.module('blogApp.PostCtrl', [])
-  .controller('PostCtrl',['$scope', '$http', '$routeParams', function($scope,$http,$routeParams){
-    $http.get('data/posts.json').success(function(data){
-      $scope.post = data[$routeParams.id];
-    });
-  }]);
--- ngBlog/app/scripts/controllers/project.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-
-angular.module('blogApp.ProjectCtrl', [])
-  .controller('ProjectCtrl',[ function(){
-
-  }]);
