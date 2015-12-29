63c8988b4a42754ca88965ce25fee34fc2108eed
Michael Funk <mafunk92@gmail.com>
2015-12-28 18:53:52 -0800

    add new ocat site

---
+++ ocat/assets/js/functions.js
@@ -0,0 +1,43 @@
+$(function() {
+  smoothScroll(300);
+  mobileNav();
+});
+
+
+function mobileNav() {
+  $('.mobile-nav-toggle').on('click', function(){
+    var status = $(this).hasClass('is-open');
+    if(status){ $('.mobile-nav-toggle, .mobile-nav').removeClass('is-open'); }
+    else { $('.mobile-nav-toggle, .mobile-nav').addClass('is-open'); }
+  });
+}
+
+
+function smoothScroll (duration) {
+	$('a[href^="#"]').on('click', function(event) {
+
+	    var target = $( $(this).attr('href') );
+
+	    if( target.length ) {
+	        event.preventDefault();
+	        $('html, body').animate({
+	            scrollTop: target.offset().top
+	        }, duration);
+	    }
+	});
+}
+
+var main = function() {
+  $('current.description').show();
+
+  $('.article').click(function() {
+      $('.article').removeClass('current');
+      $('.description').hide();
+
+      $(this).addClass('current');
+      $(this).children('.description').toggle();
+      $(this).scrollIntoView();
+    });
+  }
+
+$(document).ready(main);
