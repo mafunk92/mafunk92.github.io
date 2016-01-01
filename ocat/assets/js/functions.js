c08ae20f3938ca8ebafc37d98834956639aa6cef
Michael Funk <mafunk92@gmail.com>
2016-01-01 09:41:21 -0800

    got rid of svg logo

--- ocat/assets/js/functions.js
+++ ocat/assets/js/functions.js
@@ -118,7 +118,6 @@ function initMap() {
 
 
 $(function() {
-  svg();
   smoothScroll(300);
   mobileNav();
 });
@@ -130,40 +129,6 @@ function mobileNav() {
     if(status){ $('.mobile-nav-toggle, .mobile-nav').removeClass('is-open'); }
     else { $('.mobile-nav-toggle, .mobile-nav').addClass('is-open'); }
   });
-}
-
-function svg() {
-    var s = document.getElementById("svg-img");
-    s.setAttribute("fill","000000");
-    
-    jQuery('img.svg').each(function(){
-            var $img = jQuery(this);
-            var imgID = $img.attr('id');
-            var imgClass = $img.attr('class');
-            var imgURL = $img.attr('src');
-
-            jQuery.get(imgURL, function(data) {
-                // Get the SVG tag, ignore the rest
-                var $svg = jQuery(data).find('svg');
-
-                // Add replaced image's ID to the new SVG
-                if(typeof imgID !== 'undefined') {
-                    $svg = $svg.attr('id', imgID);
-                }
-                // Add replaced image's classes to the new SVG
-                if(typeof imgClass !== 'undefined') {
-                    $svg = $svg.attr('class', imgClass+' replaced-svg');
-                }
-
-                // Remove any invalid XML tags as per http://validator.w3.org
-                $svg = $svg.removeAttr('xmlns:a');
-
-                // Replace image with new SVG
-                $img.replaceWith($svg);
-
-            }, 'xml');
-
-        });
 }
 
 function smoothScroll (duration) {
